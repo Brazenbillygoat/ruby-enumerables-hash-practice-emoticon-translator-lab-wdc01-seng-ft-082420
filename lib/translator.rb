da536446821cbf0b89f9aae7880d8329dc9c6d25
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-12 00:37:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,8 +11,9 @@ def load_library(file_path)
     return emoticons_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emote)
   # code goes here
+  
 end
 
 def get_english_meaning
