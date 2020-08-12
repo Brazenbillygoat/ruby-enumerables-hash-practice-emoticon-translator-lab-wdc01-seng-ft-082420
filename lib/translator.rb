be7e25743d5e9a85acea2dc3bd80b2237730812d
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-12 00:51:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,6 +25,15 @@ def get_japanese_emoticon(file_path, text)
 
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, text)
   # code goes here
+  emoticons_hash = load_library(file_path)
+
+  emoticons_hash.each do |key, value|
+    if value[:japanese] == text
+      return value[:english]
+    end
+
+  end
+
 end
