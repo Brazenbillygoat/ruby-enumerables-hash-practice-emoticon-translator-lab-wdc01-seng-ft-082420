de6e9046c40f3a2301630b99f7176d3e5240905d
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-12 00:35:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,14 @@
 # require modules here
 require "yaml"
 
+def load_library(file_path)
+    emoticons_hash = YAML.load_file( file_path )
 
-def load_library(file)
-  # code goes here
+    emoticons_hash.each do |key, value|
+        emoticons_hash[key] = {english: value[0], japanese: value[1]}
+    end
+
+    return emoticons_hash
 end
 
 def get_japanese_emoticon
